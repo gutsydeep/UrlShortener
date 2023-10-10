f989ccf04f62635a1603477196e4bb68b16f2eee
Subhadeep Dutta <subhadeep.btech.ec19@iiitranchi.ac.in>
2023-10-10 23:53:43 +0530

    Made change to raise PR

--- src/main/java/com/example/Subhadeep/UrlShortener/Interceptor/RequestValidationInterceptor.java
+++ src/main/java/com/example/Subhadeep/UrlShortener/Interceptor/RequestValidationInterceptor.java
@@ -3,7 +3,6 @@ package com.example.Subhadeep.UrlShortener.Interceptor;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.web.servlet.HandlerInterceptor;
-import org.springframework.web.servlet.ModelAndView;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -13,7 +12,7 @@ public class RequestValidationInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         boolean isValid = validateRequest(request);
-        System.out.println("Request is valid: " + isValid);
+        System.out.println("Request is valid  " + isValid);
         return isValid;
     }
 
